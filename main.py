e6f3f5b80984e8f89344927a6066e69f3610b77e
Violetashes <blaz.gluk@gmail.com>
2024-11-23 18:29:47 +0100

    added rotation to actor

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill('black')
-		actor.draw(screen)
-		pygame.display.flip()
 		dt = clock.tick(60)/1000
+		actor.update(dt)
+		actor.draw(screen)		
+		pygame.display.flip()		
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")

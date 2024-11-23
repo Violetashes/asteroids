c83544350f91940418c80bf6aee28e76170e52db
Violetashes <blaz.gluk@gmail.com>
2024-11-23 17:33:07 +0100

    added basic render functionality

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from constants import * 
+import constants 
 import pygame
+import player
 
 def main():
 	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+	dt = 0
+	clock = pygame.time.Clock()
+	x = constants.SCREEN_WIDTH/2
+	y = constants.SCREEN_HEIGHT/2
+	actor = player.Player(x, y, constants.PLAYER_RADIUS)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill('black')
+		actor.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60)/1000
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")

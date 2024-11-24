5bc27bb9ef61f32754e41e003c2ca6dd580b3932
Violetashes <blaz.gluk@gmail.com>
2024-11-24 12:50:56 +0100

    final version of the game

--- asteroid.py
+++ asteroid.py
@@ -19,8 +19,10 @@ class Asteroid(CircleShape):
             return
         else:
             angle = random.uniform(20, 50)
-            left_vector = pygame.math.Vector2.rotate(self.velocity, -angle) * 1.2
-            right_vector = pygame.math.Vector2.rotate(self.velocity, angle) * 1.2
+            left_vector = pygame.math.Vector2.rotate(self.velocity, -angle)
+            right_vector = pygame.math.Vector2.rotate(self.velocity, angle)
             new_radius = self.radius - ASTEROID_MIN_RADIUS
-            left_asteroid = Asteroid(self.position, new_radius, left_vector)
-            right_asteroid = Asteroid(self.position, new_radius, right_vector)
+            left_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            left_asteroid.velocity = left_vector * 1.2
+            right_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            right_asteroid.velocity = right_vector * 1.2
